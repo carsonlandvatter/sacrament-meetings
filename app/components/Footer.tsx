da81654e1a040a85b26ba4b24574a3d558edfff2
Carson Landvatter <landvattercarson@gmail.com>
2026-07-17 14:59:51 -0600

    Address Peer Review Feedback: Make the footer stick to the bottom of the page.

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,3 +1,7 @@
 export default function Footer() {
-    return <p className="m-6">Carson Landvatter &copy;2026</p>
+    return (
+        <footer className="p-6">
+            <p>Carson Landvatter &copy;2026</p>
+        </footer>
+    );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col text-center"><Header />
-      {children}
+      <main className="flex-1">{children}</main>
       <Footer />
       </body>
     </html>
